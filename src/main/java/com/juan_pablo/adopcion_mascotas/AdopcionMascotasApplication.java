1e950861889a700be7c760549e8478fe79f34958
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-10-11 21:40:06 -0400

    proyecto inicial

---
+++ src/main/java/com/juan_pablo/adopcion_mascotas/AdopcionMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.juan_pablo.adopcion_mascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopcionMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopcionMascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan_pablo/adopcion_mascotas/AdopcionMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan_pablo.adopcion_mascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopcionMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
