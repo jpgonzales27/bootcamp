bbe9d8643aa5e52318ee66c34b653280f7487864
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-10-11 22:41:29 -0400

    Add dummy data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,22 +1,21 @@
 -- Insertar tipos de mascotas
-INSERT INTO PetType (name) VALUES ('Perro');
-INSERT INTO PetType (name) VALUES ('Gato');
-INSERT INTO PetType (name) VALUES ('Conejo');
-INSERT INTO PetType (name) VALUES ('Ave');
+INSERT INTO pet_type (type_name) VALUES ('Perro');
+INSERT INTO pet_type (type_name) VALUES ('Gato');
+INSERT INTO pet_type (type_name) VALUES ('Conejo');
+INSERT INTO pet_type (type_name) VALUES ('Ave');
 
 -- Insertar mascotas
-INSERT INTO pet (name, age, available, petType_id) VALUES ('Dogui', 3, true, 1);
-INSERT INTO mascota (name, age, available, petType_id) VALUES ('Mila', 2, true, 2);
-INSERT INTO mascota (name, age, available, petType_id) VALUES ('Nala', 1, true, 3);
-INSERT INTO mascota (name, age, available, petType_id) VALUES ('Scooby', 4, false, 4);
-INSERT INTO mascota (name, age, available, petType_id) VALUES ('Kaiser', 5, true, 1);
+INSERT INTO pet (name, age, available, pet_type_id) VALUES ('Dogui', 3, true, 1);
+INSERT INTO pet (name, age, available, pet_type_id) VALUES ('Mila', 2, true, 2);
+INSERT INTO pet (name, age, available, pet_type_id) VALUES ('Nala', 1, true, 3);
+INSERT INTO pet (name, age, available, pet_type_id) VALUES ('Scooby', 4, false, 4);
+INSERT INTO pet (name, age, available, pet_type_id) VALUES ('Kaiser', 5, true, 1);
+
 -- Insertar usuarios
-INSERT INTO usuario (id, nombre, correo_electronico, telefono) VALUES
-(1, 'Juan Pérez', 'juan.perez@example.com', '555-1234'),
-(2, 'Ana Gómez', 'ana.gomez@example.com', '555-5678'),
-(3, 'Carlos López', 'carlos.lopez@example.com', '555-9012');
+INSERT INTO user (name, email, phone) VALUES ('Juan Gonzales', 'juan.gonzales@gmail.com', '555-1234');
+INSERT INTO user (name, email, phone) VALUES ('Daniela Santa Cruz', 'dani.santacruz@gmail.com', '555-5678');
+INSERT INTO user (name, email, phone) VALUES ('Melissa lopez', 'melissa.lopez@gmail.com', '555-9012');
 
 -- Insertar adopciones
-INSERT INTO adopcion (id, mascota_id, usuario_id, fecha_adopcion) VALUES
-(1, 4, 2, '2023-09-15'), -- Tweety fue adoptado por Ana Gómez
-(2, 2, 1, '2023-10-01'); -- Miau fue adoptado por Juan Pérez
+INSERT INTO adoption (pet_id, user_id, adoption_date) VALUES (4, 2, '2023-09-15');
+INSERT INTO adoption (pet_id, user_id, adoption_date) VALUES(2, 1, '2023-10-01'); -- Miau fue adoptado por Juan Pérez
