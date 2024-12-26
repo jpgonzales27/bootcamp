711870dae16dda216e61dba30049d015db32c868
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-25 22:55:24 -0400

    agregando hateoas y aplicado en pet Type

--- src/main/java/com/juan_pablo/adopcion_mascotas/controller/PetTypeController.java
+++ src/main/java/com/juan_pablo/adopcion_mascotas/controller/PetTypeController.java
@@ -10,12 +10,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/type")
@@ -27,15 +30,29 @@ public class PetTypeController {
 
     @Operation(summary = "Get all pet types", description = "Retrieve a list of all pet types available in the system.")
     @GetMapping()
-    public List<GetPetTypeDTO> getPetType() {
-        return petTypeService.findAllPetTypes();
+    public List<EntityModel<GetPetTypeDTO>> getPetType() {
+        List<GetPetTypeDTO> petTypes = petTypeService.findAllPetTypes();
+        return petTypes.stream()
+                .map(petType -> {
+                    EntityModel<GetPetTypeDTO> petTypeModel = EntityModel.of(petType);
+                    Link selfLink = linkTo(methodOn(PetTypeController.class).getTypeById(petType.getId())).withSelfRel();
+                    petTypeModel.add(selfLink);
+                    return petTypeModel;
+                })
+                .collect(Collectors.toList());
     }
 
     @Operation(summary = "Get pet type by ID", description = "Retrieve details of a specific pet type by its unique ID.")
     @GetMapping("/{id}")
-    public ResponseEntity<PetType> getTypeById(@PathVariable Long id){
+    public ResponseEntity<EntityModel<PetType>> getTypeById(@PathVariable Long id){
         try {
-            return ResponseEntity.ok(petTypeService.findPetTypeById(id));
+            PetType petType = petTypeService.findPetTypeById(id);
+            EntityModel<PetType> petTypeModel = EntityModel.of(petType);
+            Link selfLink = linkTo(methodOn(PetTypeController.class).getTypeById(id)).withSelfRel();
+            Link allpetTypeLink = linkTo(methodOn(PetTypeController.class).getPetType()).withRel("petTypes");
+            petTypeModel.add(selfLink, allpetTypeLink);
+
+            return ResponseEntity.ok(petTypeModel);
         } catch (ObjectNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
@@ -43,18 +60,30 @@ public class PetTypeController {
 
     @Operation(summary = "Create a new pet type", description = "Add a new pet type to the system.")
     @PostMapping()
-    public ResponseEntity<PetType> createType(@Valid @RequestBody PetType petType, HttpServletRequest request) {
+    public ResponseEntity<EntityModel<PetType>> createType(@Valid @RequestBody PetType petType, HttpServletRequest request) {
         PetType petTypeCreated = petTypeService.savePetType(petType);
-        String baseUrl = request.getRequestURL().toString();
-        URI newLocation = URI.create(baseUrl + "/" + petTypeCreated.getId());
-        return ResponseEntity.created(newLocation).body(petTypeCreated);
+
+        EntityModel<PetType> PetTypeModel = EntityModel.of(petTypeCreated);
+        Link selfLink = linkTo(methodOn(PetTypeController.class).getTypeById(petTypeCreated.getId())).withSelfRel();
+        Link petTypeLink = linkTo(methodOn(PetTypeController.class).getPetType()).withRel("petTypes");
+        PetTypeModel.add(selfLink, petTypeLink);
+
+        return ResponseEntity.ok(PetTypeModel);
+//        String baseUrl = request.getRequestURL().toString();
+//        URI newLocation = URI.create(baseUrl + "/" + petTypeCreated.getId());
+//        return ResponseEntity.created(newLocation).body(petTypeCreated);
     }
 
     @Operation(summary = "Update a pet type by ID", description = "Update the details of an existing pet type by its unique ID.")
     @PutMapping("/{id}")
-    public ResponseEntity<PetType> updateType(@PathVariable Long id,@Valid  @RequestBody PetType petType) {
+    public ResponseEntity<EntityModel<PetType>> updateType(@PathVariable Long id,@Valid  @RequestBody PetType petType) {
         try {
-            return ResponseEntity.ok(petTypeService.updatePetTypeById(id, petType));
+            PetType petTypeUpdate = petTypeService.updatePetTypeById(id, petType);
+            EntityModel<PetType> petTypeModel = EntityModel.of(petTypeUpdate);
+            Link selfLink = linkTo(methodOn(PetTypeController.class).getTypeById(id)).withSelfRel();
+            petTypeModel.add(selfLink);
+
+            return ResponseEntity.ok(petTypeModel);
         } catch (ObjectNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
